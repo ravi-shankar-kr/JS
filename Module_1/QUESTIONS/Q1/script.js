d13780495588ef8f12f5b051e03cf3f2d19b4b7f
Ravi_Shankar_kr <ravi896959@gmail.com>
2026-01-17 19:05:44 +0530

    13 question done

---
+++ Module_1/QUESTIONS/Q1/script.js
@@ -0,0 +1,97 @@
+// Q1. let use karke ek variable banao jisme tumhara name ho, phir console me print karo.
+
+let fullName = "Ravi Kashyap";
+console.log(fullName);
+
+// Q2. const use karke PI banao aur uski value change karke error dikhao.
+
+const PI = 3.14;
+console.log(PI);
+
+// Q3. Ek string, number aur boolean banao aur unka typeof print karo.
+
+let str = "Ravi";
+let a = 20;
+let istrue = true;
+
+console.log(typeof(str));
+console.log(typeof(a));
+console.log(typeof(istrue));
+
+// Q4. Ek variable banao jisme null ho aur typeof check karo. Output kya aaya?
+let  c =  null;
+console.log(typeof(c));
+
+
+// Q5. "100" string ko number me convert karke usme 50 add karo.
+let hundred =  Number("100")
+let add = hundred + 50;
+console.log(add);
+console.log(typeof(hundred));
+
+// Q6. true ko number me convert karo aur output print karo.
+let True = Number(true)
+console.log(typeof(True));
+
+
+// Q7. Do numbers lo aur >, <, === ka use karke compare karo.
+
+let x = 10;
+let y = 20;
+let z = "10";
+console.log(x > y);
+console.log(x < y);
+console.log((  x === z));
+
+
+//8. Check karo ki "5" == 5 aur "5" === 5 ka output kya hai (code likho).
+
+console.log("5" == 5);
+console.log("5" === 5);
+
+// Q9. prompt() se user ka age lo aur console me print karo.
+// let Name =  prompt();
+// console.log(Name);
+
+
+// Q10. confirm() use karke user se poochho “Are you sure?” aur result print karo.
+// let isSure = confirm("Are you sure?");
+
+// Q11. Age input lo aur if-else se check karo user adult hai ya nahi.
+// let age = prompt();
+// if(age> 18){
+//     console.log("You are adult");
+// } else {
+//     console.log("tum bacche ho abhi");
+    
+// }
+
+
+
+// Q12. Upar wale question ko ternary operator se likho.
+// let age = prompt();
+// console.log(age > 18? "you are adult" : "you are child");
+
+
+
+// Q13. User ke age aur hasID variable ke base pe access allow/deny karo.
+// let age = prompt();
+// let hasID = True;
+// if(age > 18 && hasID){
+//     console.log("you are allowed"); 
+// }else{
+//     console.log("you are not allowed");    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
